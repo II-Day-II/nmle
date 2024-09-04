00aeedb289a337677596f24c5319c2354a4df261
David Kaméus <david.kameus@gmail.com>
2024-09-04 19:06:50 +0200

    some pseudo initialization

--- src/radiance_cascades/cascades.rs
+++ src/radiance_cascades/cascades.rs
@@ -1,4 +1,21 @@
+use vek::{Vec2};
 pub struct RadianceCascades {
-    p0: u8,
-    q0: u8,
+    start_interval: f32,
+    cascade_count: u8,
+}
+
+impl RadianceCascades {
+    pub fn new(screen_size: Vec2<f32>) -> Self {
+        let branching_factor = 2.0f32;
+        let interval0 = 4.0; // TODO: what should this be?
+        let diagonal = screen_size.distance(Vec2::new(0.0,0.0)); // no length()?
+        let factor = (diagonal / interval0).log(branching_factor).ceil(); 
+        let start_interval = (interval0 * branching_factor.powf(factor)) / (branching_factor - 1.0);
+        let cascade_count = start_interval.log(branching_factor).ceil() as u8;
+        Self {
+            start_interval,
+            cascade_count
+        }
+    }
+    
 }
